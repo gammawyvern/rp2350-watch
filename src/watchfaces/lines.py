a186a4f2fa3f369ad61a0aa4a0908c440cafcc19
Keagen <keegenjt@gmail.com>
2025-07-02 02:14:24 -0400

    Add numbers to watch face

--- src/watchfaces/lines.py
+++ src/watchfaces/lines.py
@@ -4,35 +4,58 @@ import math
 from adafruit_display_shapes.line import Line
 
 from watch import GC9A01ADisplay
+from utils import create_clock_number_elements
 
 
 display = GC9A01ADisplay()
 
-ARM_LENGTH = 20
 CENTER_X = 120
 CENTER_Y = 120
 
-line = Line(
-    CENTER_X, CENTER_Y,
-    CENTER_X, CENTER_Y,
-    color=0xFFFFFF
-)
+ROTATION_OFFSET = -math.pi / 2
 
-display.add("seconds", line)
+number_elements = create_clock_number_elements()
+for number in number_elements:
+    display.add(f"number_{number.text}", number)
 
-while True:
-    seconds = time.monotonic() % 60
-    minute_percent = seconds / 60
-    rotation = minute_percent * math.pi
+def create_hand(angle, length):
+    x = CENTER_X + (length * math.cos(angle))
+    y = CENTER_Y + (length * math.sin(angle))
 
-    x = CENTER_X + (ARM_LENGTH * math.cos(rotation))
-    y = CENTER_Y + (ARM_LENGTH * math.sin(rotation))
-
-    next_line = Line(
-        CENTER_X, CENTER_Y,
+    return Line(
+        int(CENTER_X), int(CENTER_Y),
         int(x), int(y),
         color=0xFFFFFF
     )
 
-    display.update("seconds", next_line)
+second_hand = create_hand(0, 0) 
+minute_hand = create_hand(0, 0)
+hour_hand = create_hand(0, 0)
+
+display.add("s", second_hand)
+display.add("m", minute_hand)
+display.add("h", hour_hand)
+
+while True:
+    total_seconds = time.monotonic()
+
+    seconds = int(total_seconds) % 60
+    minute_percent = seconds / 60
+    second_hand_angle = ROTATION_OFFSET + (2 * math.pi * minute_percent)
+
+    minutes = (total_seconds / 60) % 60
+    hour_percent = minutes / 60
+    minute_hand_angle = ROTATION_OFFSET + (2 * math.pi * hour_percent)
+
+    hours = (total_seconds / 3600) % 12
+    half_day_percent = hours / 12
+    hour_hand_angle = ROTATION_OFFSET + (2 * math.pi * half_day_percent)
+
+    new_second_hand = create_hand(second_hand_angle, 100)
+    new_minute_hand = create_hand(minute_hand_angle, 75)
+    new_hour_hand = create_hand(hour_hand_angle, 50)
+
+    display.update("s", new_second_hand)
+    display.update("m", new_minute_hand)
+    display.update("h", new_hour_hand)
 
